1192308c3b7cd533b8e952220747cf49c31b669d
JLim9269 <Ace_LT@hotmail.com>
2019-02-26 15:45:33 +0900

    LEZ GO

---
+++ Glitch Garden/Assets/Scripts/Level.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour {
+
+    // Configuration Parameters
+    [SerializeField] float delayInSeconds = 5f;
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitAndLoad());
+        }
+    }
+
+    private void LoadSplashScreen()
+    {
+        SceneManager.LoadScene("Splash Screen");
+    }
+
+    private IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
