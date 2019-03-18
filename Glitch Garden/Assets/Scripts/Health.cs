f679fb0450b7226c69ae79928238963eb68ed395
JLim9269 <Ace_LT@hotmail.com>
2019-03-18 23:27:21 +0900

    phew

---
+++ Glitch Garden/Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    // Configuration Parameters
+    [SerializeField] float health = 100;
+
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    // Configuration Parameters
+    [SerializeField] float currentSpeed = 1f;
+    [SerializeField] float damage = 100f;
+
+	void Update ()
+    {
+        transform.Translate(Vector2.right * currentSpeed * Time.deltaTime);
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Attacker")
+        {
+            Destroy(gameObject);
+            var health = other.GetComponent<Health>();
+            health.DealDamage(damage);
+        }
+    }
+}
